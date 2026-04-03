1eee76351d12cf9c665ebc669411f64237731880
crzTosser3 <alexeyrnd28@yandex.ru>
2026-04-04 00:11:20 +0300

    edit profile

--- main.py
+++ main.py
@@ -7,34 +7,12 @@ app = FastAPI(title="ШТЕУД")
 app.mount("/", StaticFiles(directory="frontend/out", html=True), name="static")
 
 @app.post("/api/profile/edit")
-async def update_profile(payload: dict):
-    """
-    Входные данные:
-    {
-        "current_profile": {
-            "nickname": "string",
-            "email": "string",
-            "password_hash": "string"
-        },
-        "new_profile": {
-            "nickname": "string",
-            "email": "string",
-            "password_hash": "string",
-            "photo_base64": "string"
-        }
-    }
-
-    Выходные данные:
-    403 - В случае если данные не валидные
-    200 - В случае успеха 
-    """    
-    
+def update_profile(payload: dict, new_payload: dict):
     return {
         "status": 200,
         "updated_profile": {
-            "nickname": "new_name",
-            "email": "new_email",
-            "message": "edited info",
-            "photo": "new_photo"
+            "nickname": new_payload["nickname"],
+            "email": new_payload["email"],
+            "photo": new_payload["photo"]
         }
     }
