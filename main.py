d716accb2fced3ba2487d6c648ea1bc90691a8da
crzTosser3 <alexeyrnd28@yandex.ru>
2026-04-04 00:20:01 +0300

    stats profile

--- main.py
+++ main.py
@@ -15,5 +15,13 @@ def update_profile(email: str, password: str, emailNew: str = None, passwordNew:
         }
     }
 
+@app.get("/api/profile/stats")
+def statistic_profile():
+    return {
+        "status" : "status",
+        "gotten_task": "gotten_task",
+        "try": "try"
+    }
+
 app.mount("/", StaticFiles(directory="frontend/out", html=True), name="static")
 
