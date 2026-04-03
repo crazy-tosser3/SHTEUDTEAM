7f61371899d16fb8fd0bdc0f9bb9695833795e6a
nikitatatata <tt.nikita.s@gmail.com>
2026-04-04 00:19:58 +0300

    fix update_profile

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="ШТЕУД")
 
-
-app.mount("/", StaticFiles(directory="frontend/out", html=True), name="static")
-
 @app.post("/api/profile/edit")
-def update_profile(payload: dict, new_payload: dict):
+def update_profile(email: str, password: str, emailNew: str = None, passwordNew: str = None, loginNew: str = None, photoNew: str = None):
     return {
         "status": 200,
         "updated_profile": {
-            "nickname": new_payload["nickname"],
-            "email": new_payload["email"],
-            "photo": new_payload["photo"]
+            "login": loginNew,
+            "email": emailNew,
+            "password": passwordNew,
+            "photo": photoNew
         }
     }
+
+app.mount("/", StaticFiles(directory="frontend/out", html=True), name="static")
+
