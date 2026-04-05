13ea2dae1ff391b8827eef78deadd960ff0cb1cc
nikitatatata <tt.nikita.s@gmail.com>
2026-04-05 12:13:50 +0300

    docker fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ['*'],
 };
 
 export default nextConfig;
